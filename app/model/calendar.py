17ef356374cad0073ea61cfc3e635e040e60f491
pedro hernandez <phernandez804@soyudemdellin.edu.co>
2024-09-12 13:50:07 -0500

    Add class Day

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field
 from datetime import datetime, date, time
 from typing import ClassVar
+from datetime import date, time, timedelta
+from app.services.util import slot_not_available_error, event_not_found_error
 
 from app.services.util import generate_unique_id, date_lower_than_today_error, event_not_found_error, \
     reminder_not_found_error, slot_not_available_error
@@ -37,4 +39,48 @@ class Event:
         return (f"ID: {self.id}\n"
                 f"Event title: {self.title}\n"
                 f"Description: {self.description}\n"
-                f"Time: {self.start_at} - {self.end_at}")            
+                f"Time: {self.start_at} - {self.end_at}")
+
+class Day:
+    def _init_(self, date_: date):
+        self.date_ = date_
+        self.slots = {}
+        self._init_slots()
+
+    def _init_slots(self):
+        current_time = time(0, 0)
+        while current_time < time(23, 45):
+            self.slots[current_time] = None
+            current_time = (datetime.combine(self.date_, current_time)+timedelta(minutes=15)).time()
+        self.slots[time(23, 45)] = None
+
+    def add_event(self, event_id: str, start_at: time, end_at: time):
+        for slot in self.slots:
+            if start_at <= slot < end_at:
+                if self.slots[slot] is not None: 
+                    slot_not_available_error()  
+        for slot in self.slots:
+            if start_at <= slot < end_at:
+                self.slots[slot] = event_id
+
+    def delete_event(self, event_id: str):
+        deleted = False
+        for slot, saved_id in self.slots.items():
+            if saved_id == event_id:
+                self.slots[slot] = None
+                deleted = True
+        if not deleted:
+            event_not_found_error() 
+
+    def update_event(self, event_id: str, start_at: time, end_at: time):
+        for slot in self.slots:
+            if self.slots[slot] == event_id:
+                self.slots[slot] = None  
+        for slot in self.slots:
+            if start_at <= slot < end_at:
+                if self.slots[slot]:
+                 slot_not_available_error()
+            else:
+                self.slots[slot] = event_id
+
+              
