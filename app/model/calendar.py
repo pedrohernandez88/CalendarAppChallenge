55d43a209e1d7dab10524155666eb6b0e3ec066d
pedro hernandez <phernandez804@soyudemdellin.edu.co>
2024-09-12 13:25:25 -0500

    Add class Event

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -14,4 +14,27 @@ class Reminder:
     def __str__(self):
         return f"Reminder on {self.date_time} of type {self.type}"
     
- 
+class Event:
+    title: str
+    description: str
+    date_: date
+    start_at: time
+    end_at: time
+    id: str = field(default_factory=generate_unique_id)
+    reminders: list ['Reminder'] = field(default_factory=list)
+
+    def add_reminder(self, date_time, type_="email"):
+        reminder = Reminder(date_time=date_time, type=type_)
+        self.reminders.append(reminder)
+
+    def delete_reminder(self, reminder_index: int):
+        if 0 <= reminder_index < len(self.reminders):
+            self.reminders.pop(reminder_index)
+        else:
+            reminder_not_found_error(reminder_index)
+
+    def __str__(self):
+        return (f"ID: {self.id}\n"
+                f"Event title: {self.title}\n"
+                f"Description: {self.description}\n"
+                f"Time: {self.start_at} - {self.end_at}")            
